e5ec187e3ffaf2c3659b52a2c0dad48c703c6982
bleak <ffeeaarrofdeath@mail.ru>
2023-07-10 18:26:22 +0300

    commit

---
+++ src/store/actions-creators/user.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { UserAction, userActionTypes } from "../../types/user";
+import { Dispatch } from "redux";
+
+export const fetchUsers = () => {
+  return async (dispatch: Dispatch<UserAction>) => {
+    try {
+      dispatch({ type: userActionTypes.FETCH_USERS });
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setTimeout(() => {
+        dispatch({
+          type: userActionTypes.FETCH_USERS_SUCCESS,
+          payload: response.data,
+        });
+      }, 500);
+    } catch (e) {
+      dispatch({
+        type: userActionTypes.FETCH_USERS_ERROR,
+        payload: "Произошла ошибка при загрузке пользователей",
+      });
+    }
+  };
+};
